1d03c09202eece520faa5265f69e8ce22de0b37a
Ayumi_Work <walkerimai@gmail.com>
2019-12-20 18:20:50 +0900

    test

--- langs/java/Free.java
+++ langs/java/Free.java
@@ -1,3 +1,10 @@
 public class Free {
+    // メソッドの引数に＜＞を用いた場合と、メソッド名の前に＜＞を付けた場合の違いは？
 
+    // java.util.ArrayList と java.util.Array$ArrayList (2 つは非互換) の違い
+    // → 後者はインナークラス
+
+    // ArrayList(Collection<? extends E> C) は何してんの？
+
+    // LinkedList<E> は複数のインターフェースを実装してる。
 }
